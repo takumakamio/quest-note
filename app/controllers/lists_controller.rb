fb57e98f8c9be3f7a332e6b67406866cdf0d96e0
EC2 Default User <ec2-user@ip-172-31-42-167.ap-northeast-1.compute.internal>
2021-07-05 16:21:33 +0000

    first commit

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -1,6 +1,10 @@
 class ListsController < ApplicationController
   before_action :set_list, only: %i(edit update destroy)
 
+  def index
+    @lists = List.where(user: current_user).order("created_at ASC")
+  end
+
   def new
     @list = List.new
 
@@ -9,7 +13,7 @@ class ListsController < ApplicationController
   def create
     @list = List.new(list_params)
     if @list.save
-      redirect_to quests_path
+      redirect_to lists_path
     else
       render :new
     end
@@ -22,7 +26,7 @@ class ListsController < ApplicationController
   def update
 
     if @list.update_attributes(list_params)
-      redirect_to quests_path
+      redirect_to lists_path
     else
       render action: :edit
     end
@@ -30,11 +34,12 @@ class ListsController < ApplicationController
 
   def destroy
       @list.destroy
-      redirect_to quests_path
+      redirect_to lists_path
   end
 
 
  private
+    # merge　FKを外部に渡すために必要
     def list_params
       params.require(:list).permit(:title).merge(user: current_user)
     end
